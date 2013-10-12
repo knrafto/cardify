8a3b7ca81cee0eb8933da041b7e771b66e3a62a7
Kyle Raftogianis <kylerafto@gmail.com>
2013-10-11 19:48:00 -0700

    Test Graph API

---
+++ src/cardify.js
@@ -0,0 +1,33 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: "648090275223045",
+    status: true,
+    xfbml: true
+  });
+
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    if (response.status === 'connected') {
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      FB.login();
+    } else {
+      FB.login();
+    }
+  });
+};
+
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Good to see you, ' + response.name + '.');
+  });
+}
+
+// Load the SDK asynchronously
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
